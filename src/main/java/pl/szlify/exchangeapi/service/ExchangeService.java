908b41b33486493c398078ef5fa443c07437d748
Tczeq <tczekanowski@gmail.com>
2024-03-02 17:54:10 +0100

    first commit

---
+++ src/main/java/pl/szlify/exchangeapi/service/ExchangeService.java
@@ -0,0 +1,19 @@
+package pl.szlify.exchangeapi.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import pl.szlify.exchangeapi.model.TestModel;
+import pl.szlify.exchangeapi.properties.ExchangeApiProperties;
+
+@Service
+@RequiredArgsConstructor
+public class ExchangeService {
+
+    private final ExchangeApiProperties properties;
+
+    public TestModel test() {
+        return new TestModel(properties.getBaseUrl(), properties.getApiKey());
+    }
+}

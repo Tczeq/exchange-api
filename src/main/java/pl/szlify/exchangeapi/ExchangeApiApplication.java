6c8474e02b0c5078516ed978d3e6ea1c9d1f8405
Tczeq <tczekanowski@gmail.com>
2024-03-10 21:36:09 +0100

    added cache

--- src/main/java/pl/szlify/exchangeapi/ExchangeApiApplication.java
+++ src/main/java/pl/szlify/exchangeapi/ExchangeApiApplication.java
@@ -3,10 +3,12 @@ package pl.szlify.exchangeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import pl.szlify.exchangeapi.properties.ExchangeApiProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ExchangeApiProperties.class)
+@EnableCaching
 public class ExchangeApiApplication {
 
     public static void main(String[] args) {

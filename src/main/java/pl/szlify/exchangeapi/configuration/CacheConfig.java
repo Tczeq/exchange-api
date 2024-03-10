aeec586dcf28d68499ad03626feb1cba2a0a889e
Tczeq <tczekanowski@gmail.com>
2024-03-10 21:36:09 +0100

    added cache

---
+++ src/main/java/pl/szlify/exchangeapi/configuration/CacheConfig.java
@@ -0,0 +1,31 @@
+package pl.szlify.exchangeapi.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+
+
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("cacheSymbols");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .initialCapacity(200) // określa poczatkową pojemnosc cache
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .maximumSize(10000)
+                .recordStats()); //wlacza rejestrowanie statystyk przy uzyciu cache
+        return cacheManager;
+    }
+
+
+}
--- src/main/java/pl/szlify/exchangeapi/controller/ExchangeController.java
+++ src/main/java/pl/szlify/exchangeapi/controller/ExchangeController.java
@@ -1,32 +1,36 @@
 package pl.szlify.exchangeapi.controller;
 
-import feign.Param;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.szlify.exchangeapi.configuration.CacheConfig;
 import pl.szlify.exchangeapi.model.ConvertRequest;
 import pl.szlify.exchangeapi.model.ConvertResponse;
 import pl.szlify.exchangeapi.model.SymbolsDto;
 import pl.szlify.exchangeapi.service.ExchangeService;
 
-import java.math.BigDecimal;
-
 @RestController
 @RequestMapping("/api/exchange")
 @RequiredArgsConstructor
+@Slf4j
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
-
+    @Cacheable(cacheNames = "cacheSymbols")
     @GetMapping("/symbols")
     public ResponseEntity<SymbolsDto> getSymbols() {
         SymbolsDto symbolsDto = exchangeService.getAllSymbols();
         return new ResponseEntity<>(symbolsDto, HttpStatus.OK);
     }
+
 
     @GetMapping("/convert")
     public ResponseEntity<ConvertResponse> convert(@Valid ConvertRequest convertRequest) {
@@ -34,16 +38,5 @@ public class ExchangeController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-//    @GetMapping("/test")
-//    public TestModel get() {
-//        return exchangeService.test();
-//    }
-//
-//    @GetMapping("/test-response")
-//    public ResponseEntity<TestModel> getResponse() {
-//        TestModel testModel = exchangeService.test();
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .header("name", "values")
-//                .body(testModel);
-//    }
+
 }

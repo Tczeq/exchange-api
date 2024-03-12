1da974d3f1bd9024dcf0726e0ec163378349835f
Tczeq <tczekanowski@gmail.com>
2024-03-13 00:55:14 +0100

    added open feign

--- src/main/java/pl/szlify/exchangeapi/client/ExchangeClient.java
+++ src/main/java/pl/szlify/exchangeapi/client/ExchangeClient.java
@@ -2,18 +2,31 @@ package pl.szlify.exchangeapi.client;
 
 import feign.Param;
 import feign.RequestLine;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.szlify.exchangeapi.model.ConvertResponse;
 import pl.szlify.exchangeapi.model.SymbolsDto;
 
 import java.math.BigDecimal;
 
 
-//@FeignClient(name = "api-service", url = "${exchange.api.api-key}")
+@FeignClient(name = "api-service", url = "${exchange.api.api-key}")
 public interface ExchangeClient {
-    @RequestLine("GET /symbols")
+
+
+    //stare
+//    @RequestLine("GET /symbols")
+//    SymbolsDto findAll();
+//
+//    @RequestLine("GET /convert?from={from}&to={to}&amount={amount}")
+//    ConvertResponse convert(@Param("from") String from, @Param("to") String to, @Param("amount")BigDecimal amount);
+
+    //nowe
+
+    @GetMapping("/symbols")
     SymbolsDto findAll();
 
-    @RequestLine("GET /convert?from={from}&to={to}&amount={amount}")
-    ConvertResponse convert(@Param("from") String from, @Param("to") String to, @Param("amount")BigDecimal amount);
-
+    @GetMapping("/convert")
+    ConvertResponse convert(@RequestParam("from") String from, @RequestParam("to") String to, @RequestParam("amount") BigDecimal amount);
 }

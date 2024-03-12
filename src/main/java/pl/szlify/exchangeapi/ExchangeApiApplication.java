85ce1e0aa9b2a671b3c5b3bc571f31e76a55247d
Tczeq <tczekanowski@gmail.com>
2024-03-13 00:55:14 +0100

    added open feign

--- src/main/java/pl/szlify/exchangeapi/ExchangeApiApplication.java
+++ src/main/java/pl/szlify/exchangeapi/ExchangeApiApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import pl.szlify.exchangeapi.properties.ExchangeApiProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ExchangeApiProperties.class)
 @EnableCaching
+@EnableFeignClients
 public class ExchangeApiApplication {
 
     public static void main(String[] args) {

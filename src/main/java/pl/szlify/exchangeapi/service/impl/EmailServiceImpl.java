9c4f0d847a04425881ebe7a70513a5d5d60908fe
Tczeq <tczekanowski@gmail.com>
2024-03-17 22:06:09 +0100

    added email content exchange

--- src/main/java/pl/szlify/exchangeapi/service/impl/EmailServiceImpl.java
+++ src/main/java/pl/szlify/exchangeapi/service/impl/EmailServiceImpl.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Service;
 import pl.szlify.exchangeapi.model.ConvertResponse;
 import pl.szlify.exchangeapi.service.EmailService;
 
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
@@ -14,24 +18,62 @@ public class EmailServiceImpl implements EmailService {
     private static final String SUBJECT = "Exchange confirmation";
 
     private final JavaMailSender emailSender;
+    private final String getEmailUsername; //Bean
 
-    @Override public void sendConfirmation(String to, ConvertResponse convertResponse) {
+    @Override
+    public void sendConfirmation(String to, ConvertResponse convertResponse) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("exchangeapka@gmail.com");
+        message.setFrom(getEmailUsername);
         message.setTo(to);
         message.setSubject(SUBJECT);
-        message.setText("DUPA"); //TODO: utworzyć metodę, która na bazie convertResponse utworzy treść maila
+        message.setText(createEmailContent(convertResponse)); //TODO: utworzyć metodę, która na bazie convertResponse utworzy treść maila
         emailSender.send(message);
     }
 
     @Override
     public void sendConf(String to) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("exchangeapka@gmail.com");
-        message.setTo(to);
-        message.setSubject(SUBJECT);
-        message.setText("DUPA"); //TODO: utworzyć metodę, która na bazie convertResponse utworzy treść maila
-        emailSender.send(message);
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom(getEmailUsername);
+//        message.setTo(to);
+//        message.setSubject(SUBJECT);
+//        message.setText("DUPA"); //TODO: utworzyć metodę, która na bazie convertResponse utworzy treść maila
+//        emailSender.send(message);
     }
 
+    public String createEmailContent(ConvertResponse convertResponse) {
+        StringBuilder emailContent = new StringBuilder();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate date = LocalDate.parse(convertResponse.getDate());
+        emailContent.append("Potwierdzenie wymiany walut,")
+                .append("\n")
+                .append("z dnia: ")
+                .append(date.format(formatter))
+                .append("\n\n");
+        emailContent.append("Sukces: ")
+                .append(convertResponse.isSuccess() ? "Tak" : "Nie")
+                .append("\n\n");
+
+        DecimalFormat formatRate = new DecimalFormat("0.00");
+        emailContent.append("Wymiana:")
+                .append("\n");
+        emailContent.append(" - ")
+                .append(convertResponse.getQuery().getFrom())
+                .append("/")
+                .append(convertResponse.getQuery().getTo())
+                .append("\n");
+        emailContent.append(" - Ilość: ")
+                .append(convertResponse.getQuery().getAmount())
+                .append("\n");
+        emailContent.append(" - Kurs: ")
+                .append(formatRate.format(convertResponse.getInfo().getRate()))
+                .append("\n");
+
+        DecimalFormat format = new DecimalFormat("#.##");
+        emailContent.append("Wynik transakcji: ")
+                .append(format.format(convertResponse.getResult()))
+                .append(" ")
+                .append(convertResponse.getQuery().getTo());
+
+        return emailContent.toString();
+    }
 }

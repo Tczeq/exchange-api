8dc321e0abacfd453a1e6e2ee82b89a98d33a84c
Tczeq <tczekanowski@gmail.com>
2024-03-18 19:24:51 +0100

    added oauth2

---
+++ src/main/java/pl/szlify/exchangeapi/security/SecurityConfig.java
@@ -0,0 +1,97 @@
+package pl.szlify.exchangeapi.security;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{bcrypt}$2a$10$m8D4KGHnY5KHRz0bcUlF/eapHPGoP3uviDpRQt/4rqMCxcyyV8Umu") //test123
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{bcrypt}$2a$10$m8D4KGHnY5KHRz0bcUlF/eapHPGoP3uviDpRQt/4rqMCxcyyV8Umu") //test123
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{bcrypt}$2a$10$m8D4KGHnY5KHRz0bcUlF/eapHPGoP3uviDpRQt/4rqMCxcyyV8Umu") //test123
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeHttpRequests(configurer ->
+//                configurer
+//                        .requestMatchers(HttpMethod.GET, "/api/exchange/symbols").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.GET, "/api/exchange/convert/**").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.GET, "/api/exchange/convert/**").hasRole("ADMIN")
+//
+//        );
+//        httpSecurity.httpBasic(Customizer.withDefaults())
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .exceptionHandling(e -> e.accessDeniedHandler(accessDeniedHandler()));
+//
+////        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+//
+//        return httpSecurity.build();
+//    }
+//
+//    @Bean
+//    public AccessDeniedHandler accessDeniedHandler() {
+//        return (request, response, e) -> {
+//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//            response.setContentType("application/json;charset=UTF-8");
+//            response.getWriter().write("{\"error\": \"Access denied - You don't have necessary permissions.\"}");
+//        };
+//    }
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults()) // Włączenie logowania OAuth 2.0
+//                .oauth2Client(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+    }
+
+
+
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests(authorize -> authorize
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2Login(Customizer.withDefaults());
+//    }
+
+}
